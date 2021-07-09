f7e9a3612d8791171f00ad84d161f04b0be17863
Gabriel da Gama <gabriel@gabrielgama.com.br>
2021-07-09 23:59:30 +0100

    Update charCommand.ts

--- src/commands/charCommand.ts
+++ src/commands/charCommand.ts
@@ -5,16 +5,39 @@ import axios, {AxiosResponse} from 'axios';
 export default class CharCommand implements CommandInterface {
     command:string = 'char';
 
-    async execute(ctx: Context): Promise<void> {
+    execute(ctx: Context): void {
         const char = ctx.message?.text.replace('/char', '')
         if (char) {
-            const charInfo = await axios.get(`https://api.tibiadata.com/v2/characters/${char}.json`).then((res: AxiosResponse) => res.data.characters.data).catch((err) => {
-                ctx.reply('Invalid name.')
-            })
+            axios.get(`https://api.tibiadata.com/v2/characters/${char}.json`).then(function (res: AxiosResponse) {
+            const charInfo = res.data.characters.data;
             if (!charInfo || !charInfo.name) {
                 ctx.reply('Invalid name.')
             }
-            ctx.reply('Funciona', {reply_to_message_id: ctx.message?.message_id, parse_mode: 'HTML'})
+            const vocationIcon = () => {
+                switch (charInfo.vocation) {
+                    case 'Elder Druid' : return '❄'
+                    case 'Druid' : return '❄'
+                    case 'Royal Paladin' : return '🏹'
+                    case 'Paladin' : return '🏹'
+                    case 'Elite Knight' : return '🛡'
+                    case 'Knight' : return '🛡'
+                    case 'Sorcerer' : return '🔥'
+                    case 'Master Sorcerer' : return '🔥'
+                }
+            }
+    
+            const charResponse = `${charInfo.name} ${charInfo.sex === 'male' ? '♂' : '♀'}
+    <b>Vocation:</b> ${charInfo.vocation} ${vocationIcon()}
+    <b>Level:</b> ${charInfo.level}
+    <b>World:</b> ${charInfo.world}
+    <b>Residence:</b> ${charInfo.residence}
+    <b>Guild:</b> ${charInfo.guild?.name ? `${charInfo.guild?.rank} of ${charInfo.guild?.name}` : '-'}
+    <b>Status:</b> ${charInfo.status} ${charInfo.status === 'online' ? '🟢' : '🔴'}
+    <a href="https://www.tibia.com/community/?subtopic=characters&name=${charInfo.name}">See more</a>`
+            ctx.reply(charResponse, {parse_mode: "HTML", disable_web_page_preview: true, reply_to_message_id: ctx.message?.message_id})
+        }).catch((err) => {
+                ctx.reply('Invalid name.')
+            })
         } else {
             ctx.reply('Try use "<pre>/char [name]</pre>"', {reply_to_message_id: ctx.message?.message_id, parse_mode: 'HTML'})
         }
