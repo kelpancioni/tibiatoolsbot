11b5747390f337409723e9ef3cc5d14289e4e12f
Gabriel da Gama <gabriel@gabrielgama.com.br>
2021-07-10 00:03:08 +0100

    Update charCommand.ts

--- src/commands/charCommand.ts
+++ src/commands/charCommand.ts
@@ -5,11 +5,12 @@ import axios, {AxiosResponse} from 'axios';
 export default class CharCommand implements CommandInterface {
     command:string = 'char';
 
-    execute(ctx: Context): void {
+    async execute(ctx: Context): Promise<void> {
         const char = ctx.message?.text.replace('/char', '')
         if (char) {
-            axios.get(`https://api.tibiadata.com/v2/characters/${char}.json`).then(function (res: AxiosResponse) {
-            const charInfo = res.data.characters.data;
+            const charInfo = await axios.get(`https://api.tibiadata.com/v2/characters/${char}.json`).then((res: AxiosResponse) => res.data.characters.data).catch((err) => {
+                ctx.reply('Invalid name.')
+            })
             if (!charInfo || !charInfo.name) {
                 ctx.reply('Invalid name.')
             }
@@ -35,9 +36,6 @@ export default class CharCommand implements CommandInterface {
     <b>Status:</b> ${charInfo.status} ${charInfo.status === 'online' ? '🟢' : '🔴'}
     <a href="https://www.tibia.com/community/?subtopic=characters&name=${charInfo.name}">See more</a>`
             ctx.reply(charResponse, {parse_mode: "HTML", disable_web_page_preview: true, reply_to_message_id: ctx.message?.message_id})
-        }).catch((err) => {
-                ctx.reply('Invalid name.')
-            })
         } else {
             ctx.reply('Try use "<pre>/char [name]</pre>"', {reply_to_message_id: ctx.message?.message_id, parse_mode: 'HTML'})
         }
