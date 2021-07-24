7f804f7136ff0d915401b1f2c43ae493b05c9acd
Gabriel da Gama <gabriel@gabrielgama.com.br>
2021-07-24 11:54:54 +0100

    Fixed issue with webhookReply

--- src/commands/charCommand.ts
+++ src/commands/charCommand.ts
@@ -2,18 +2,19 @@ import { Context } from 'telegraf/typings';
 import CommandInterface from './commandInterface';
 import Character from '../entity/character';
 import TibiaDataApi from './../api/tibiaDataApi';
+import { Message } from 'telegraf/typings/telegram-types';
 
 export default class CharCommand implements CommandInterface {
     command:string = 'char';
 
-    execute(ctx: Context): void {
+    execute(ctx: Context): Promise<Message> {
         const char = ctx.message?.text.replace('/char', '').trim();
         if (char) {
-            TibiaDataApi.getCharData(char).then(
+            return TibiaDataApi.getCharData(char).then(
                 (charObject:Character) => ctx.reply(this.buildResponse(charObject),{parse_mode: "HTML", disable_web_page_preview: true, reply_to_message_id: ctx.message?.message_id})
             ).catch(() =>ctx.reply('Try use "<pre>/char [name]</pre>"', {reply_to_message_id: ctx.message?.message_id, parse_mode: 'HTML'}));
         } else {
-            ctx.reply('Try use "<pre>/char [name]</pre>"', {reply_to_message_id: ctx.message?.message_id, parse_mode: 'HTML'})
+            return ctx.reply('Try use "<pre>/char [name]</pre>"', {reply_to_message_id: ctx.message?.message_id, parse_mode: 'HTML'})
         }
     }
 
